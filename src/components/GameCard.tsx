a99834d0dd6ccfa4090190e0a01ce586928c1888
unknown <vignesh.p@auckland.ac.nz>
2025-02-25 10:06:47 +1100

    Create card and populate the fetched data

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,11 +1,24 @@
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import { Game } from "../hooks/useGames";
 
 interface Props {
   game: Game;
 }
 
-const GameCard = () => {
-  return <div>GameCard</div>;
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card>
+      <CardMedia
+        component="img"
+        height="140"
+        image={game.background_image}
+        alt={game.name}
+      />
+      <CardContent>
+        <Typography variant="h5">{game.name}</Typography>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default GameCard;
